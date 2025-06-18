4d517178d931cec6ac7f3b59aaf2714110043b59
Raul Catalan <raulcatalan95@gmail.com>
2025-06-18 19:42:36 -0400

    prueba

--- comunidad-infeliz/src/App.tsx
+++ comunidad-infeliz/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div> 
         <h1>Comunidad Infelizzzz</h1>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
